c5b25b00f1a3ffb8dca09861f210793341a398d0
Narunart Wongsawat <58408563+narunartpml@users.noreply.github.com>
2024-05-17 19:31:01 +0700

    feat(restapi): init CRUD web app
    - doc update
    - invoice generation to set item base on accountManagementFeeItem

--- exam1/restlet/CreateInvoice.js
+++ exam1/restlet/CreateInvoice.js
@@ -26,27 +26,71 @@ define(["N/record", "N/search"], function (record, search) {
       });
       //TODO: add validation functions
       invoice.setValue({ fieldId: "entity", value: customerId });
-      invoice.setValue({ fieldId: "trandate", value: data.tranDate });
       invoice.setValue({ fieldId: "memo", value: data.memo });
       invoice.setValue({ fieldId: "tranid", value: data.tranId });
       invoice.setValue({ fieldId: "externalid", value: data.tranId });
-      //TODO: find Account Management Fee to charge the Customer
+
+      let accountManagementFeeItem = CreateInvoice.findItemFee(customerId);
       invoice.selectNewLine({ sublistId: "item" });
       invoice.setCurrentSublistValue({
         sublistId: "item",
         fieldId: "item",
-        value: data.item,
+        value: accountManagementFeeItem.item,
       });
       invoice.setCurrentSublistValue({
         sublistId: "item",
         fieldId: "amount",
-        value: data.amount,
+        value: accountManagementFeeItem.amount,
       });
       invoice.commitLine({ sublistId: "item" });
+      invoice.setValue({
+        fieldId: "trandate",
+        value: accountManagementFeeItem.processDate,
+      });
       let invoiceId = invoice.save();
       return { success: true, invoiceId: invoiceId };
     } catch (e) {
       return { success: false, message: e.message };
+    }
+  };
+
+  /**
+   * Finds the account management fee item for the customer.
+   * @param {string} customerId - The internal id of the customer.
+   * @returns {Object} - The account management fee item.
+   * @returns {string} item - The internal id of the item.
+   * @returns {string} amount - The amount of the item.
+   * @returns {string} processDate - The process date of the item.
+   * @throws {Error} - If the account management fee is not found.
+   */
+  CreateInvoice.findItemFee = function (customerId) {
+    let itemFeeSearch = search.create({
+      type: "customrecord_account_management_fee",
+      filters: [
+        [
+          "custrecord_account_responsible_party",
+          search.Operator.IS,
+          customerId,
+        ],
+      ],
+      columns: [
+        "custrecord_account_management_item",
+        "custrecord_account_management_item_amount",
+        "custrecord_account_management_fee_process_date",
+      ],
+    });
+    let result = itemFeeSearch.run().getRange({ start: 0, end: 1 });
+    if (result.length > 0) {
+      //TODO: update to handle multiple items
+      return {
+        item: result[0].getValue("custrecord_account_management_item"),
+        amount: result[0].getValue("custrecord_account_management_item_amount"),
+        processDate: result[0].getValue(
+          "custrecord_account_management_fee_process_date"
+        ),
+      };
+    } else {
+      throw new Error("Account Management Fee not found");
     }
   };
 
